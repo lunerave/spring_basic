b0f17388c8aa3adbc4cc9672193202b2e5c847e5
lunerave <lunerave07@naver.com>
2023-10-28 18:16:45 +0900

    Implements member domain and test

---
+++ src/main/java/hellospring/core/MemberApp.java
@@ -0,0 +1,20 @@
+package hellospring.core;
+
+import hellospring.core.member.Grade;
+import hellospring.core.member.Member;
+import hellospring.core.member.MemberService;
+import hellospring.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("findMember = " + findMember.getName());
+        System.out.println("new member = " + member.getName());
+
+    }
+}
