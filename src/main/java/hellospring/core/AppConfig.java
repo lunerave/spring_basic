854ed19d2eda39ebbe96e0e6ebf6564ec64783e2
lunerave <lunerave07@naver.com>
2023-11-05 20:12:55 +0900

    Refactor AppConfig

--- src/main/java/hellospring/core/AppConfig.java
+++ src/main/java/hellospring/core/AppConfig.java
@@ -2,7 +2,9 @@ package hellospring.core;
 
 import hellospring.core.Order.OrderService;
 import hellospring.core.Order.OrderServiceImpl;
+import hellospring.core.discount.DiscountPolicy;
 import hellospring.core.discount.FixDiscountPolicy;
+import hellospring.core.member.MemberRepository;
 import hellospring.core.member.MemberService;
 import hellospring.core.member.MemberServiceImpl;
 import hellospring.core.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import hellospring.core.member.MemoryMemberRepository;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
