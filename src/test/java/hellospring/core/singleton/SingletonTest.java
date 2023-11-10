daa176964b0702b1e8aea4e41a1f812746144bcd
lunerave <lunerave07@naver.com>
2023-11-10 14:23:02 +0900

    Add test: Spring container provides singleton pattern for the objects

--- src/test/java/hellospring/core/singleton/SingletonTest.java
+++ src/test/java/hellospring/core/singleton/SingletonTest.java
@@ -5,6 +5,7 @@ import hellospring.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -38,5 +39,21 @@ public class SingletonTest {
         System.out.println("singletonService2 = " + singletonService2);
 
         assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        //스프링은 자동으로 객체들을 싱글톤으로 관리를 해준다
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
