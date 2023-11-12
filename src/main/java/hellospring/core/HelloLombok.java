cedbe36c7e33299ec860b1e373d0ca4c779352f4
lunerave <lunerave07@naver.com>
2023-11-12 19:49:36 +0900

    Apply lombok

---
+++ src/main/java/hellospring/core/HelloLombok.java
@@ -0,0 +1,24 @@
+package hellospring.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("sdss");
+
+//        String name = helloLombok.getName();
+//        System.out.println("name = " + name);
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
--- src/main/java/hellospring/core/Order/OrderServiceImpl.java
+++ src/main/java/hellospring/core/Order/OrderServiceImpl.java
@@ -6,10 +6,12 @@ import hellospring.core.discount.RateDiscountPolicy;
 import hellospring.core.member.Member;
 import hellospring.core.member.MemberRepository;
 import hellospring.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //final에 대해서 생성자를 자동으로 만들어준다
 public class OrderServiceImpl implements OrderService{
 
     // DIP 원칙을 지키기 위해서 추상체에만 의존하고 구현체에는 의존하지 않도록 수정한다.
@@ -31,11 +33,12 @@ public class OrderServiceImpl implements OrderService{
 //    }
 
     // 생성자 의존 관계주입을 권장함: final을 쓸 수 있고 테스트에 더 편리하다
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired // 생성자가 하나 일때는 생략가능
+    // @RequiredArgsConstructor 롬복 사용으로 생성자 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
