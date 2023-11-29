25595e6e9d4e954ffe3614617b6db20d16b2557b
lunerave <lunerave07@naver.com>
2023-11-29 20:26:23 +0900

    Test: using annotation @PostConstruct, @PreDestroy

--- src/test/java/hellospring/core/lifecycle/NetworkClient.java
+++ src/test/java/hellospring/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hellospring.core.lifecycle;
 
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,12 +31,14 @@ public class NetworkClient {
 
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
